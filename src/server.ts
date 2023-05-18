21feb90d4d405a44e77bf0f65e67ff7e809dd958
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-17 22:32:10 -0500

    Add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import express from 'express';
+import router from './routes/realestate';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
+
+// allows everyone to access the api
+app.use(cors())
+// whenever a request is made to the server, it will be logged to the console
+app.use(morgan('dev'))
+// whenever a request is made to the server, it will be parsed to JSON
+app.use(express.json())
+// it will format the querystring to an object
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.status(200)
     res.json({message: 'Hello World!'});
 });
+
+app.use('/api', router);
 
 export default app
