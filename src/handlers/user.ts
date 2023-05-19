82aced68f7d7e2b19942b4c67f70afd311da2525
Gonzalo Barrazueta <68360681+gonzalobarrazueta@users.noreply.github.com>
2023-05-19 16:57:16 -0500

    Create functions to sign up and sign in

---
+++ src/handlers/user.ts
@@ -0,0 +1,37 @@
+import prisma from "../db"
+import { comparePasswords, createJWT, hashPassword } from "../modules/auth"
+
+// Creates a user/ sign up
+export const createUser = async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email,
+            password: await hashPassword(req.body.password),
+            phone: req.body.phone,
+            age: req.body.age,
+            homeAdress: req.body.homeAdress
+        }
+    })
+
+    const token = createJWT(user)
+    res.json({ token })
+}
+
+export const signIn = async (req, res) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            email: req.body.email
+        }
+    })
+
+    const isValid = await comparePasswords(req.body.password, user.password)
+
+    if (!isValid) {
+        res.status(401)
+        res.json({ message: 'Invalid password' })
+    }
+
+    const token = createJWT(user)
+    res.json({ token })
+}
